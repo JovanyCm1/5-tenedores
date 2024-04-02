3aa7d25cfcc1877216185d22b4fe192a1445c012
JovanyCm1 <jaime.clasess@gmail.com>
2024-04-02 14:05:58 -0600

    Video 70 - Validando y controlando el formulario de cambio de email

--- src/components/Account/ChangeEmailForm/ChangeEmailForm.js
+++ src/components/Account/ChangeEmailForm/ChangeEmailForm.js
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 import { View } from "react-native";
 import { Input, Button } from "react-native-elements";
 import { styles } from "./ChangeEmailForm.styles";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./ChangeEmailForm.data";
 
 export function ChangeEmailForm(props) {
   const { onClose, onReload } = props;
   const [showPassword, setShowPassword] = useState(false);
 
   const onShowPassword = () => setShowPassword((prevState) => !prevState);
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
   return (
     <View style={styles.content}>
-      <Input placeholder="Nuevo email" containerStyle={styles.input} />
+      <Input
+        placeholder="Nuevo email"
+        containerStyle={styles.input}
+        onChangeText={(text) => formik.setFieldValue("email", text)}
+        errorMessage={formik.errors.email}
+      />
       <Input
         placeholder="Contraseña"
         containerStyle={styles.input}
@@ -21,11 +37,15 @@ export function ChangeEmailForm(props) {
           color: "#c2c2c2",
           onPress: onShowPassword,
         }}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        errorMessage={formik.errors.password}
       />
       <Button
         title="Cambiar email"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
       />
     </View>
   );
