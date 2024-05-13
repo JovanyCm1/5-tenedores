6fc8798e2ea3fba6301e8a563d102ccd8543d115
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2024-05-13 03:25:12 -0600

    Se cambio la ubicacion de la firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAz26CzNnVg8oHgkiBOD2ciAk8tyBsxWy0",
-  authDomain: "tenedores-v2-e0203.firebaseapp.com",
-  projectId: "tenedores-v2-e0203",
-  storageBucket: "tenedores-v2-e0203.appspot.com",
-  messagingSenderId: "1020027196673",
-  appId: "1:1020027196673:web:b5c08252b57beb30a27700",
+  apiKey: "AIzaSyBGUWzWQjcIdmTsKkML7-bYYsAuV6e7JNg",
+  authDomain: "canias-415118.firebaseapp.com",
+  projectId: "canias-415118",
+  storageBucket: "canias-415118.appspot.com",
+  messagingSenderId: "1046326776810",
+  appId: "1:1046326776810:web:04a15930500a067ffd9893"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const initFirebase = initializeApp(firebaseConfig);
