00b728e344bb5e0d3e98066d1e9c4531f8a41a9e
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2024-05-21 02:10:17 -0600

    Ahora la pantalla de dispositivos muestra los datos la locacion actual

--- src/screens/Dispositivos/DispositivosScreen.js
+++ src/screens/Dispositivos/DispositivosScreen.js
@@ -1,11 +1,40 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import { styles } from './DispositivosScreen.style'
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
 export function DispositivosScreen() {
+    const [weatherData, setWeatherData] = useState(null);
+    const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+    console.log(location.latitude, location.longitude);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.error('Permission to access location was denied');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+            const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=de70cd52f01093abc88283a19af4fc3f&units=metric`);
+            setWeatherData(response.data);
+        })();
+    }, []);
+
     return (
-        <ScrollView>
-            <Text>SearchScreen</Text>
-        </ScrollView>
-    )
+        <View>
+            {weatherData ? (
+                <View>
+                    <Text>Location: {location.latitude}, {location.longitude}</Text>
+                    <Text>Temperatura: {weatherData.main.temp}°C</Text>
+                    <Text>Humedad: {weatherData.main.humidity}%</Text>
+                    <Text>Presión: {weatherData.main.pressure} hPa</Text>
+                    <Text>Velocidad del viento: {weatherData.wind.speed} m/s</Text>
+                </View>
+            ) : (
+                <Text>Cargando...</Text>
+            )}
+        </View>
+    );
 }
